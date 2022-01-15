5d6d759d5f67c55c92355eb5e6144700152bec56
IJLAL AHMAD <ijlalahmad845@gmail.com>
2022-01-15 16:01:35 +0530

    designed fully functional spirit level

--- app/src/main/java/com/example/spiritlevel/MainActivity.kt
+++ app/src/main/java/com/example/spiritlevel/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.spiritlevel
 
+import android.R.attr
 import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -8,16 +9,29 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.R.attr.button
+import android.content.res.ColorStateList
+
+import android.graphics.drawable.Drawable
+
+
+
 
 class MainActivity : AppCompatActivity(),SensorEventListener {
-    lateinit var sensorManager: SensorManager
-    lateinit var textView: TextView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var fab1: FloatingActionButton
+    private lateinit var fab2: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView=findViewById(R.id.spirit)
+        fab1=findViewById(R.id.spirit)
+        fab2=findViewById(R.id.spirit2)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         sensorManager=getSystemService(SENSOR_SERVICE) as SensorManager
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
@@ -35,16 +49,22 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
             val sides=p0.values[0]
             val upDown=p0.values[1]
 
-            textView.apply {
-                rotationX=upDown*3f
-                rotationY=sides*3f
+            fab1.apply {
+                rotationX=upDown*2f
+                rotationY=sides*2f
                 rotation=-sides
-                translationX=sides*-10
-                translationY=upDown*10
+                translationX=sides*-3
+                translationY=upDown*3
+            }
+
+            fab2.apply {
+                translationX=sides*-30
+                translationY=upDown*30
             }
 
             val color=if(sides.toInt()==0 && upDown.toInt()==0)Color.GREEN else Color.RED
-            textView.setBackgroundColor(color)
+
+            fab2.backgroundTintList= ColorStateList.valueOf(color)
         }
     }
 
